8ff143252239d83aa1a82d3e0344548d88e9b891
John Ciliberti <johnciliberti@users.noreply.github.com>
2016-12-01 01:45:03 -0500

    all samples pass smoke test

---
+++ Chapter03/Recipe01/src/Recipe01/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recipe01.Models
+{
+    /// <summary>
+    /// Summary description for Class
+    /// </summary>
+    public class Product
+    {
+	    public int ProductId { get; set; }
+    }
+}
