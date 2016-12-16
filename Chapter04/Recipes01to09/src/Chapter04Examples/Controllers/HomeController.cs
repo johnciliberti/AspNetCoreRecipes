10c06331f0f96e3cf6f45b2620037a4ac31b60de
John Ciliberti <johnciliberti@users.noreply.github.com>
2016-12-15 21:40:40 -0500

    Recipe 05 completed

--- Chapter04/Recipes01to09/src/Chapter04Examples/Controllers/HomeController.cs
+++ Chapter04/Recipes01to09/src/Chapter04Examples/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Chapter04.Models.Recipe03;
+﻿using Chapter04.Models.Recipe03;
 using Chapter04.Models.Recipe04;
+using Chapter04.Models.Recipe05;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Chapter04.Controllers
 {
@@ -21,6 +25,7 @@ namespace Chapter04.Controllers
             return View();
         }
 
+        #region Recipe 3
         public IActionResult Recipe03()
         {
             var model = new Contact { AllowContactAboutOffers = true };
@@ -38,7 +43,9 @@ namespace Chapter04.Controllers
             }
             return View(model);
         }
+        #endregion
 
+        #region Recipe 4
         public IActionResult Recipe04()
         {
             Tristate model = new Tristate { NullableBoolValue = null };
@@ -51,6 +58,44 @@ namespace Chapter04.Controllers
             return View(model);
 
         }
+        #endregion
+
+        #region Recipe 5
+        private List<SelectListItem> _items = new List<SelectListItem>
+        {
+           new SelectListItem { Value="", Text="Please Select a Brand"},
+           new SelectListItem { Value="1", Text="Gibson" },
+           new SelectListItem { Value="2", Text="Charvel" },
+           new SelectListItem { Value="3", Text="Ibenez" },
+           new SelectListItem { Value="4", Text="Jackson"  }
+        };
+
+        public IActionResult Recipe05()
+        {
+            var model = new GuitarBrandViewModel { Brands=_items};
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Recipe05(GuitarBrandViewModel model)
+        {
+            model.Brands = _items;
+            if (model.SelectedBrandId != 0)
+            {
+                model.SelectedBrand = (from b in _items
+                                       where b.Value == model.SelectedBrandId.ToString()
+                                       select new GuitarBrand
+                                       {
+                                           GuitarBrandId = int.Parse(b.Value),
+                                           Name = b.Text
+                                       }).FirstOrDefault();
+
+            }
+
+            return View(model);
+        }
+
+        #endregion
 
         public IActionResult Error()
         {
