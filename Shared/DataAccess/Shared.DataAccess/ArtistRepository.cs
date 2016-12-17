ab927c1ab8ae5641b1e98f5911cacfe9a96ecf38
John Ciliberti <johnciliberti@users.noreply.github.com>
2016-12-17 13:47:20 -0500

    Added info to create NuGet package

--- Shared/DataAccess/Shared.DataAccess/ArtistRepository.cs
+++ Shared/DataAccess/Shared.DataAccess/ArtistRepository.cs
@@ -3,6 +3,9 @@ using System.Linq;
 
 namespace AspNetCoreMvcRecipes.Shared.DataAccess
 {
+    /// <summary>
+    /// Repository that simplifies accessing and modifing artist information
+    /// </summary>
     public class ArtistRepository : Repository<Artist>
     {
         public ArtistRepository(MoBContext context) : base(context)
@@ -10,6 +13,10 @@ namespace AspNetCoreMvcRecipes.Shared.DataAccess
 
         }
 
+        /// <summary>
+        /// Get a list of the last 20 artists to register for the site.
+        /// </summary>
+        /// <returns>List of Artists</returns>
         public List<Artist> GetNewArtists()
         {
             var query = (from e in _context.Artists
