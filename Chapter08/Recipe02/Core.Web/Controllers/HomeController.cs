0ef956c2ff88fa7684e8f683a4c4b85cb3134dca
johnciliberti <pulverized@hotmail.com>
2017-03-12 03:07:09 -0400

    Added code for Recipe 02.

---
+++ Chapter08/Recipe02/Core.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recipe02.DataAccess.Entities;
+using Recipe02.DataAccess.Repository;
+
+namespace Recipe02.Core.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IGuitarRepository _guitarRepository;
+        public HomeController(IGuitarRepository guitarRepository)
+        {
+            _guitarRepository = guitarRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var guitarList = _guitarRepository.GetAllGuitars();
+
+            return View("Index", guitarList);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Chapter08/Recipe02/DataAccess/Entities/Guitar.cs
@@ -0,0 +1,15 @@
+﻿namespace Recipe02.DataAccess.Entities
+{
+    public class Guitar
+    {
+        public int GuitarId { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int NumberOfStrings { get; set; }
+        public GuitarBodyStyle BodyStyle { get; set; }
+        
+    }
+
+    public enum GuitarBodyStyle {Solid, Chambered, Hollow }
+}
