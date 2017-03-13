52f3d0bea947114c697bff1b50dfa97d4ca9c674
johnciliberti <pulverized@hotmail.com>
2017-03-13 01:30:35 -0400

    Completed code for Chapter 8 Recipe 4

---
+++ Chapter08/Recipe04/Recipe04.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCoreMvcRecipes.Shared.DataAccess;
+
+namespace Recipe04.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        IUnitOfWork _UnitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _UnitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var model = _UnitOfWork.ArtistRepository.GetNewArtists();
+            return View("Index", model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
