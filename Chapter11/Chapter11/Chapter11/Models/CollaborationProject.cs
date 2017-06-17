29c75f4291215e7f6d89a0111a7e497545afd640
johnciliberti <pulverized@hotmail.com>
2017-06-17 01:42:10 -0400

    Chapter 11 First Draft

---
+++ Chapter11/Chapter11/Chapter11/Models/CollaborationProject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter11.Models
+{
+    public class CollaborationProject
+    {
+        public string ProjectName { get; set; }
+        public string Status { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
---
+++ Chapter11/Chapter11/Chapter11/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Chapter11.Models
+{
+    public class Profile
+    {
+        public string ProfileImageUrl { get; set; }
+        public string BannerImageUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+
+        public IList<CollaborationProject> Projects { get; set; }
+
+    }
+}
